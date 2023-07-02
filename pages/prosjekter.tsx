692574a80b5a394159572f583dde5613fd13a09e
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-07-02 17:40:23 +0200

    Create project page

--- pages/prosjekter.tsx
+++ pages/prosjekter.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './styles/PageTemplate.module.scss';
 import Image from 'next/image';
 import image from '../components/Hero/assets/project-1.jpeg';
+import projects from '../components/Hero/data/projects';
+import projectStyles from './styles/Prosjekter.module.scss';
 
 export default function PageTemplate() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h5>Our story</h5>
-        <h1>How one womans renovation disaster transformed an industry</h1>
+        <h5>Prosjekter</h5>
+        <h1>Oversikt over et utvalg av våre prosjekter</h1>
       </div>
       <div className={styles.content}>
         <p>
@@ -32,6 +34,99 @@ export default function PageTemplate() {
           a growing network of the nation’s most qualified general contractors.
         </p>
         <Image src={image} alt="" width={400} height={500} />
+      </div>
+      <div className={projectStyles.prosjekter}>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
+        <div className={projectStyles.enkeltProsjekt}>
+          <Image
+            className={projectStyles.projectImage}
+            src={projects[0].image}
+            alt=""
+            width={300}
+            height={400}
+          />
+          <div className={projectStyles.projectText}>
+            <h6>Tittel</h6>
+            <p>Beskrivelse</p>
+          </div>
+        </div>
       </div>
     </div>
   );
