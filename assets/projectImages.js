29d0f586047b49f8f54baf1121e9733fc4f7cbd9
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-08-04 18:55:20 +0200

    Add projects json for images

---
+++ assets/projectImages.js
@@ -0,0 +1,64 @@
+const projects = [
+  {
+    id: 1,
+    name: 'Majorstua',
+    before: [
+      { id: 1, name: 'Image 1', image: 'folder1/before/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder1/before/image2.jpg' },
+    ],
+    after: [
+      { id: 1, name: 'Image 1', image: 'folder1/after/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder1/after/image2.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Tøyen',
+    before: [
+      { id: 1, name: 'Image 1', image: 'folder2/before/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/before/image2.jpg' },
+    ],
+    after: [
+      { id: 1, name: 'Image 1', image: 'folder2/after/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/after/image2.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Skillebekk',
+    before: [
+      { id: 1, name: 'Image 1', image: 'folder2/before/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/before/image2.jpg' },
+    ],
+    after: [
+      { id: 1, name: 'Image 1', image: 'folder2/after/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/after/image2.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Marienlyst',
+    before: [
+      { id: 1, name: 'Image 1', image: 'folder2/before/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/before/image2.jpg' },
+    ],
+    after: [
+      { id: 1, name: 'Image 1', image: 'folder2/after/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/after/image2.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Haugerud',
+    before: [
+      { id: 1, name: 'Image 1', image: 'folder2/before/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/before/image2.jpg' },
+    ],
+    after: [
+      { id: 1, name: 'Image 1', image: 'folder2/after/image1.jpg' },
+      { id: 2, name: 'Image 2', image: 'folder2/after/image2.jpg' },
+    ],
+  },
+];
+
+export default projects;
